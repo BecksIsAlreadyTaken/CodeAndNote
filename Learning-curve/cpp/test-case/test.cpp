27b7664f205c590ff141e4938f36a0e0b37725d6
BecksIsAlreadyTaken <503646976@qq.com>
2020-04-27 15:59:56 +0800

    Add Learning-curve.

---
+++ Learning-curve/cpp/test-case/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"PLaP"<<endl;
+    cin.ignore(); // "system("pause");" without "press any key to continue;" in linux
+    return 0;
+}

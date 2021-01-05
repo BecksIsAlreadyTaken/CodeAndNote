acc83341b02026b7ed97c980aa19ce3f15ac4dd6
BecksIsAlreadyTaken <503646976@qq.com>
2021-01-05 21:47:47 +0800

    Add machine learning week1 note and init PKUOJ 1009

---
+++ notes/CS231N/test.py
@@ -0,0 +1,17 @@
+import numpy as np
+# x = list(range(5))
+# print(x)
+# print(x[:-1])
+# print(x[1:2])
+
+# print(x[0:5])
+# print(x[0:len(x)])
+
+# y = [(y, y+1) for y in x]
+# print(y)
+
+# y = {(y, y+1): y for y in x}
+# print(y)
+
+a = np.array([1, 2]).reshape(2, 1)
+print(a)
